22c4e94bfa89b7c19830239dd4cd162f2ae4dae3
dnth <dickson.neoh@gmail.com>
2026-06-25 10:33:50 +0800

    feat: enable MLflow workspaces

--- mlflow/startup.sh
+++ mlflow/startup.sh
@@ -3,6 +3,7 @@ echo 'starting mlflow server...'
 
 exec mlflow server \
     --backend-store-uri $DB_URL \
+    --serve-artifacts \
     --artifacts-destination s3://bucket \
     --allowed-hosts "${MLFLOW_ALLOWED_HOSTS:-*}" \
     --cors-allowed-origins "${MLFLOW_CORS_ALLOWED_ORIGINS:-*}" \
